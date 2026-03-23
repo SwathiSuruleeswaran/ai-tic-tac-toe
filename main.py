3838b22084e9b5532571776733ee7060b67e0d77
Swathi Suruleeswaran <swathis23ec@psnacet.edu.in>
2026-03-23 22:36:27 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,72 @@
+Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==================================================== RESTART: C:/Users/swath/AppData/Local/Programs/Python/Python312/tic_tac_toe.py ====================================================
+
+
+  |   |  
+--+---+--
+  |   |  
+--+---+--
+  |   |  
+
+
+Enter position (0-8): 5
+
+
+  |   | O
+--+---+--
+  |   | X
+--+---+--
+  |   |  
+
+
+Enter position (0-8): 0
+
+
+X |   | O
+--+---+--
+O |   | X
+--+---+--
+  |   |  
+
+
+Enter position (0-8): 6
+
+
+X |   | O
+--+---+--
+O | O | X
+--+---+--
+X |   |  
+
+
+Enter position (0-8): 8
+
+
+X |   | O
+--+---+--
+O | O | X
+--+---+--
+X | O | X
+
+
+Enter position (0-8): 1
+
+
+X | X | O
+--+---+--
+O | O | X
+--+---+--
+X | O | X
+
+
+Draw!
+>>> 
+==================================================== RESTART: C:/Users/swath/AppData/Local/Programs/Python/Python312/tic_tac_toe.py ====================================================
+>>> 
+==================================================== RESTART: C:/Users/swath/AppData/Local/Programs/Python/Python312/tic_tac_toe.py ====================================================
+>>> 
+==================================================== RESTART: C:/Users/swath/AppData/Local/Programs/Python/Python312/tic_tac_toe.py ====================================================
+>>> 
+================================================================== RESTART: C:/Users/swath/OneDrive/Documents/main.py ==================================================================
